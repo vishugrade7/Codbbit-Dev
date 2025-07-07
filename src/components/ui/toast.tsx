6a91b8bcc26b296d3bfb41c08b61f77fec6e66c7
Vishu Grade <gradevishu@gmail.com>
2025-07-07 18:45:50 +0000

    make toast notifications glass blur also dismis them in 1.5 seconds auto

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,9 +29,10 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default:
+          "border bg-background/70 backdrop-blur-lg text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-destructive bg-destructive/70 backdrop-blur-lg text-destructive-foreground",
       },
     },
     defaultVariants: {
