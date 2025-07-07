22ded968117cf4a5e2bcf62b7db6c51735f630ea
Vishu Grade <gradevishu@gmail.com>
2025-07-07 06:38:50 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button type="subm

--- src/components/edit-profile-modal.tsx
+++ src/components/edit-profile-modal.tsx
@@ -14,7 +14,6 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -198,158 +197,160 @@ export default function EditProfileModal({ isOpen, onOpenChange, user }: EditPro
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit Profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 py-4">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                   <div className="relative">
-                        <UserIcon className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                        <Input placeholder="Your full name" {...field} className="pl-10" />
-                    </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="isEmailPublic"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                  <div className="space-y-0.5">
-                    <FormLabel>Show Email</FormLabel>
-                    <FormDescription>
-                      Allow others to see your email on your public profile.
-                    </FormDescription>
-                  </div>
-                  <FormControl>
-                    <Switch
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="company"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Company</FormLabel>
-                    <div className="relative">
-                      <FormControl>
-                          <div className="relative">
-                              <div className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none">
-                                {companyLogo && !logoError ? (
-                                    <Image
-                                      src={companyLogo}
-                                      alt="Company Logo"
-                                      width={20}
-                                      height={20}
-                                      onError={() => setLogoError(true)}
-                                    />
-                                ) : (
-                                    <Building className="h-5 w-5 text-muted-foreground" />
-                                )}
-                              </div>
-                              <Input 
-                                placeholder="Your company or college" 
-                                {...field} 
-                                className="pl-10"
-                                autoComplete="off"
-                                onFocus={() => companyValue && suggestions.length > 0 && setIsSuggestionsOpen(true)}
-                                onBlur={() => setIsSuggestionsOpen(false)}
-                              />
-                          </div>
-                      </FormControl>
-                      {isSuggestionsOpen && suggestions.length > 0 && (
-                        <Card className="absolute z-10 w-full mt-1 bg-popover border-border shadow-lg">
-                          <CardContent className="p-0">
-                            <ul className="flex flex-col">
-                              {suggestions.map((suggestion) => (
-                                <CompanySuggestionItem key={suggestion.domain} suggestion={suggestion} onClick={handleSuggestionClick} />
-                              ))}
-                            </ul>
-                          </CardContent>
-                        </Card>
-                      )}
-                    </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="trailheadUrl"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Trailhead URL</FormLabel>
-                   <div className="relative">
-                        <LinkIcon className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                        <Input placeholder="https://trailblazer.me/id/..." {...field} className="pl-10" />
-                    </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="githubUrl"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>GitHub URL</FormLabel>
-                   <div className="relative">
-                        <Github className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                        <Input placeholder="https://github.com/..." {...field} className="pl-10" />
-                    </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="linkedinUrl"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>LinkedIn URL</FormLabel>
-                   <div className="relative">
-                        <Linkedin className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                        <Input placeholder="https://linkedin.com/in/..." {...field} className="pl-10" />
-                    </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-             <FormField
-              control={form.control}
-              name="twitterUrl"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Twitter / X URL</FormLabel>
-                   <div className="relative">
-                        <Twitter className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                        <Input placeholder="https://x.com/..." {...field} className="pl-10" />
-                    </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <DialogHeader className="flex flex-row justify-between items-center">
+              <div>
+                <DialogTitle>Edit Profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+              </div>
               <Button type="submit" disabled={isLoading}>
                 {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
                 Save changes
               </Button>
-            </DialogFooter>
+            </DialogHeader>
+            <div className="space-y-4 py-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <div className="relative">
+                          <UserIcon className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                          <Input placeholder="Your full name" {...field} className="pl-10" />
+                      </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="isEmailPublic"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                    <div className="space-y-0.5">
+                      <FormLabel>Show Email</FormLabel>
+                      <FormDescription>
+                        Allow others to see your email on your public profile.
+                      </FormDescription>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="company"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company</FormLabel>
+                      <div className="relative">
+                        <FormControl>
+                            <div className="relative">
+                                <div className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none">
+                                  {companyLogo && !logoError ? (
+                                      <Image
+                                        src={companyLogo}
+                                        alt="Company Logo"
+                                        width={20}
+                                        height={20}
+                                        onError={() => setLogoError(true)}
+                                      />
+                                  ) : (
+                                      <Building className="h-5 w-5 text-muted-foreground" />
+                                  )}
+                                </div>
+                                <Input 
+                                  placeholder="Your company or college" 
+                                  {...field} 
+                                  className="pl-10"
+                                  autoComplete="off"
+                                  onFocus={() => companyValue && suggestions.length > 0 && setIsSuggestionsOpen(true)}
+                                  onBlur={() => setIsSuggestionsOpen(false)}
+                                />
+                            </div>
+                        </FormControl>
+                        {isSuggestionsOpen && suggestions.length > 0 && (
+                          <Card className="absolute z-10 w-full mt-1 bg-popover border-border shadow-lg">
+                            <CardContent className="p-0">
+                              <ul className="flex flex-col">
+                                {suggestions.map((suggestion) => (
+                                  <CompanySuggestionItem key={suggestion.domain} suggestion={suggestion} onClick={handleSuggestionClick} />
+                                ))}
+                              </ul>
+                            </CardContent>
+                          </Card>
+                        )}
+                      </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="trailheadUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Trailhead URL</FormLabel>
+                    <div className="relative">
+                          <LinkIcon className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                          <Input placeholder="https://trailblazer.me/id/..." {...field} className="pl-10" />
+                      </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="githubUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>GitHub URL</FormLabel>
+                    <div className="relative">
+                          <Github className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                          <Input placeholder="https://github.com/..." {...field} className="pl-10" />
+                      </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="linkedinUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>LinkedIn URL</FormLabel>
+                    <div className="relative">
+                          <Linkedin className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                          <Input placeholder="https://linkedin.com/in/..." {...field} className="pl-10" />
+                      </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="twitterUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Twitter / X URL</FormLabel>
+                    <div className="relative">
+                          <Twitter className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                          <Input placeholder="https://x.com/..." {...field} className="pl-10" />
+                      </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
           </form>
         </Form>
       </DialogContent>
