81af83899b3f70ac5c888b0c3dfcddb5633a8564
Vishu Grade <gradevishu@gmail.com>
2025-07-12 13:58:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -31,7 +31,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
       "data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow",
       "data-[state=inactive]:text-muted-foreground data-[state=inactive]:hover:text-foreground/80",
       className
