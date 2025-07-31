acaf2dcad553c826950c4d883b3e1a5452ab043e
Vishu Grade <gradevishu@gmail.com>
2025-07-31 06:58:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div dat

--- src/app/sheets/[sheetId]/page.tsx
+++ src/app/sheets/[sheetId]/page.tsx
@@ -510,7 +510,7 @@ export default function SheetDisplayPage() {
                                 <PanelLeft className="mr-2 h-4 w-4" /> View Details
                             </Button>
                         </SheetTrigger>
-                        <SheetContent side="bottom" className="p-0 h-4/5 rounded-t-lg">
+                        <SheetContent side="bottom" className="p-0 h-4/5 rounded-t-lg bg-background/80 backdrop-blur-sm">
                              <div className="p-4 h-full"><SheetDetails /></div>
                         </SheetContent>
                     </Sheet>
