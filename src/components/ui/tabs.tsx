a7a4c13b26af95d6a51501b09d7f765a0eaf0cdc
Vishu Grade <gradevishu@gmail.com>
2025-07-31 08:55:53 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div dir

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -32,7 +32,7 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       "relative inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1.5 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-      "data-[state=inactive]:hover:bg-background/50 data-[state=inactive]:hover:text-foreground",
+      "data-[state=inactive]:hover:text-foreground",
       "data-[state=active]:text-foreground",
       className
     )}
