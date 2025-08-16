b8afeee8f617e28fecbd189aa9e740b449073273
Vishu Grade <yt.salesforcehelp@gmail.com>
2025-08-16 12:33:39 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
+  "projectId": "showcase-canvas-rx61p",
+  "appId": "1:408120069857:web:6cd5331d496145beb3b1a0",
+  "storageBucket": "showcase-canvas-rx61p.appspot.com",
+  "apiKey": "AIzaSyCoQTrq-kldgLibBYKFf4syhrGArLmmu9g",
+  "authDomain": "showcase-canvas-rx61p.firebaseapp.com",
+  "messagingSenderId": "408120069857"
 };
 
 // Initialize Firebase
