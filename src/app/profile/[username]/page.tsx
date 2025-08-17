76f6e093520cd5a78eede97f0bf226efff62e948
Vishu Grade <gradevishu@gmail.com>
2025-08-17 23:45:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="flex-

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -260,7 +260,7 @@ export default function UserProfilePage() {
 
   return (
     <>
-    <main className="flex-1 bg-muted/40">
+    <main className="flex-1 bg-muted/40 dark:bg-black">
         <div className="container mx-auto px-4 md:px-6 py-8">
             <div className="grid grid-cols-1 md:grid-cols-12 gap-8 items-start">
                 <div className="md:col-span-12 lg:col-span-4 space-y-8">
